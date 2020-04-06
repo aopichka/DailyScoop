851519bc9310c5722fe3689ddf1dc55c2e12d059
ryanbrown <rjbrown38>
2020-04-05 20:42:45 -0500

    Created layout of homepage

--- app/src/main/java/com/example/dailyscoop/MainPage.java
+++ app/src/main/java/com/example/dailyscoop/MainPage.java
@@ -2,16 +2,16 @@ package com.example.dailyscoop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(LoginActivity.this, MainPage.class));
+        setContentView(R.layout.activity_main_page);
     }
+
+
+
 }
