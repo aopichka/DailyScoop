3b83108e52276179f30cd2716c7914d3cec5ca33
Nathan Kiraly <natekiraly@gmail.com>
2020-04-06 03:11:00 -0500

    Added permissions and layout for the home page - Ryan Brown

--- app/src/main/java/com/example/dailyscoop/HomeActivity.java
+++ app/src/main/java/com/example/dailyscoop/HomeActivity.java
@@ -4,18 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button btnLogout;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -23,15 +24,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Set up the firebase authentication
         firebaseAuth = FirebaseAuth.getInstance();
 
-        btnLogout = findViewById(R.id.btn_logout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logout();
-            }
-        });
     }
 
     private void logout() {
@@ -55,4 +50,30 @@ public class HomeActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void updatePicture(String newImage, int index) {
+
+        String uri = "@drawable/" + newImage;
+
+        int imageResource = getResources().getIdentifier(uri, null, getPackageName()); //get image  resource
+
+        Drawable res = getResources().getDrawable(imageResource); // convert into drawble
+
+        ImageView img = null;
+        if (index == 0) {
+            img = findViewById(R.id.img_rest1);
+        } else if (index == 1) {
+            img = findViewById(R.id.img_rest2);
+        } else if (index == 2) {
+            img = findViewById(R.id.img_rest3);
+        } else if (index == 3) {
+            img = findViewById(R.id.img_rest4);
+        }
+
+        if (img != null) {
+            img.setImageDrawable(res); // set as image
+        }
+
+    }
+
 }
