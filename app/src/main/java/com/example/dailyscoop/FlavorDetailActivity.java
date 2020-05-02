e9bb08606aadbd3c870e61088911f91a21c8fedc
rbrown38@wisc.edu <rbrown38@wisc.edu>
2020-05-02 15:22:03 -0500

    Filled in Location Activity and Layout

---
+++ app/src/main/java/com/example/dailyscoop/FlavorDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.dailyscoop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlavorDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flavor_detail);
+    }
+}
