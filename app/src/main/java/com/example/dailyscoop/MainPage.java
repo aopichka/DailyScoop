a29cb6f537551ccf1a7fe5f2b154364c72c3990c
rbrown38@wisc.edu <rbrown38@wisc.edu>
2020-05-01 20:12:18 -0500

    Set up homepage layout

--- app/src/main/java/com/example/dailyscoop/MainPage.java
+++ app/src/main/java/com/example/dailyscoop/MainPage.java
@@ -2,7 +2,9 @@ package com.example.dailyscoop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainPage extends AppCompatActivity {
 
@@ -13,5 +15,42 @@ public class MainPage extends AppCompatActivity {
     }
 
 
+    void updatePicture(String newImage, int index){
+
+        String uri = "@drawable/" + newImage;
+
+        int imageResource = getResources().getIdentifier(uri, null, getPackageName()); //get image  resource
+
+        Drawable res = getResources().getDrawable(imageResource); // convert into drawble
+
+        if(index == 0){
+            ImageView img = findViewById(R.id.imageView);
+            img.setImageDrawable(res); // set as image
+        }
+        if(index == 1){
+            ImageView img = findViewById(R.id.imageView2);
+            img.setImageDrawable(res); // set as image
+        }
+        if(index == 2){
+            ImageView img = findViewById(R.id.imageView3);
+            img.setImageDrawable(res); // set as image
+        }
+        if(index == 3){
+            ImageView img = findViewById(R.id.imageView4);
+            img.setImageDrawable(res); // set as image
+        }
+        if(index == 4){
+            ImageView img = findViewById(R.id.imageView5);
+            img.setImageDrawable(res); // set as image
+        }
+        if(index == 5){
+            ImageView img = findViewById(R.id.imageView6);
+            img.setImageDrawable(res); // set as image
+        }
+        if(index == 6){
+            ImageView img = findViewById(R.id.imageView7);
+            img.setImageDrawable(res); // set as image
+        }
+    }
 
 }
